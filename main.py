6d20f79391ec23c7f67cb3788df84f6766dac891
krausm-bzz <krausm@bzz.ch>
2024-08-28 11:24:47 +0200

    done

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-# Dein Code kommt hier hin
+"""
+Skript
+"""
+
+import json
+
+
+def adjust_recipe(recipe, amount_people):
+    new_ingredients = {}
+    ingredients = recipe["ingredients"]
+    servings = recipe["servings"]
+    factor = amount_people / servings
+    for ingredient, amount in ingredients.items():
+        new_ingredients[ingredient] = amount * factor
+
+    new_recipe = {
+        'title': recipe['title'],
+        'ingredients': new_ingredients,
+        'servings': amount_people
+    }
+    return new_recipe
+
+
+def load_recipe(json_recipe):
+    return json.loads(json_recipe)
+
 
 if __name__ == '__main__':
     # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    python_dict = json.loads(recipe_json)
+    original_recipe = load_recipe(recipe_json)
+    # Adjust the recipe for 2 people
+    adjusted_recipe = adjust_recipe(original_recipe, 8)
+    print(f"Original Recipe: {original_recipe}")
+    print(f"Adjusted Recipe: {adjusted_recipe}")
